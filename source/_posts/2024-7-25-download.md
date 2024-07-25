5ac1191a0fccc5da7ad0ca856c339b2c96716e04
SmartLWW <1351161980@qq.com>
2024-07-25 17:15:48 +0800

    update

---
+++ source/_posts/2024-7-25-download.md
@@ -0,0 +1,8 @@
+---
+title: download
+date: 2024-07-25 13:09:35
+tags:
+---
+下载测试
+</br>
+<a href="1.txt" download="1.txt">下载</a>
---
+++ themes/my-theme/source/js/mine.js
@@ -0,0 +1,11 @@
+function stopAudio()
+ {
+        document.getElementById('audio').pause();
+        audio.currentTime = 0; // 将播放位置重置到开头
+}
+
+function playAudio()
+{
+    document.getElementById("audio").play();
+    document.getElementById("audio").muted = false;
+}
