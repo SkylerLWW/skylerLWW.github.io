0d4748aa05ddb63c4e351dead169936d80b005cf
SmartLWW <1351161980@qq.com>
2024-08-11 17:06:06 +0800

    update

--- themes/my-theme/source/js/main.js
+++ themes/my-theme/source/js/main.js
@@ -1,17 +1,14 @@
-function stopAudio()
- {
-        document.getElementById('audio').pause();
-        audio.currentTime = 0; // 将播放位置重置到开头
+function stopAudio() {
+    document.getElementById('audio').pause();
+    audio.currentTime = 0; // 将播放位置重置到开头
 }
 
-function playAudio()
-{
+function playAudio() {
     document.getElementById("audio").play();
     document.getElementById("audio").muted = false;
 }
 
-function backTop()
-{
+function backTop() {
     event.preventDefault();
-    window.scrollTo(0, 0 ,'smooth');
+    window.scrollTo(0, 0, 'smooth');
 }
