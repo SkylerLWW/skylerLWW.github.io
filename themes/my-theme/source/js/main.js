161fb97236bddc598afb5b73d89d5580e04ad42a
SmartLWW <1351161980@qq.com>
2024-08-18 14:10:44 +0800

    update

--- themes/my-theme/source/js/main.js
+++ themes/my-theme/source/js/main.js
@@ -1,14 +1,30 @@
 function stopAudio() {
     document.getElementById('audio').pause();
-    audio.currentTime = 0; // 将播放位置重置到开头
+    document.getElementById('audio').currentTime = 0; // 将播放位置重置到开头
+    document.getElementById('stop-music').style.display = 'none';
+    document.getElementById('start-music').style.display = 'block';
 }
 
 function playAudio() {
     document.getElementById("audio").play();
     document.getElementById("audio").muted = false;
+    document.getElementById('stop-music').style.display = 'block';
+    document.getElementById('start-music').style.display = 'none';
 }
 
 function backTop() {
     event.preventDefault();
     window.scrollTo(0, 0, 'smooth');
+}
+
+function showLive2D() {
+    var element = document.getElementById('show-live_2d');
+    const directoryPath = window.location.pathname;
+    const directoryOnly = directoryPath.substring(0, directoryPath.lastIndexOf('/'));
+    if (!directoryOnly.trim()) {
+        InitLive2D()
+        element.style.display = 'none';
+    } else {
+        element.innerText = "看板娘只有主页有哦~";
+    }
 }
